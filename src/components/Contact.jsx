16dc86efe6c5b89f333ac9d56712e1794c241a2f
Randy Kipkurui <randy.kipkurui4@gmail.com>
2025-02-21 12:26:14 +0300

    ✨feat: update email service configuration in Contact component.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,16 +33,19 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        // import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        // import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        'service_xgqr1nh',
+        'template_5bbzb0h',
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Randy Kipkurui",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "randy.kipkurui4@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        // import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        'LS9IHnXeHB-U21BwQ'
       )
       .then(
         () => {
